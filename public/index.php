92dd30c703655f91d2b7840e2a1e29e295e82b50
andrey <andrej_vmire@mail.ru>
2021-10-23 23:35:36 +0300

    add UrlMatcher

--- public/index.php
+++ public/index.php
@@ -1,26 +1,37 @@
 <?php
+
+namespace App;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
 require_once "../vendor/autoload.php";
 
 ini_set("display_errors", 1);
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
 $request = Request::createFromGlobals();
 $response = new Response();
+$routeCollection = new RouteCollection();
+$routeCollection->add("hello", new Route('/hello/{name}', ["name" => "World"]));
+$routeCollection->add("bye", new Route("/bye"));
+$context = new RequestContext();
+$context->fromRequest($request);
 
-$name = $request->get("name", "World");
+$matcher = new UrlMatcher($routeCollection, $context);
 
-$maps = [
-    "/hello" => "./pages/hello.php",
-    "/bye" => "./pages/bye.php",
-];
-
-$path = $request->getPathInfo();
-if (isset($maps[$path])) {
-    require $maps[$path];
-} else {
+try {
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
+    ob_start();
+    require sprintf("./pages/%s.php", $_route);
+} catch (ResourceNotFoundException $exception) {
     $response->setStatusCode(404);
-    $response->setContent("Страница не найдена");
+    $response->setContent("Страница не  найдена");
+} catch (\Exception $exception) {
+    $response->setStatusCode(500);
 }
 $response->send();
--- public/pages/hello.php
+++ public/pages/hello.php
@@ -2,6 +2,4 @@
 
 use Symfony\Component\HttpFoundation\Response;
 
-$name = $request->get("name", "World");
-
 $response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
